941faa5005dec3b19285f33cd36244b4ddf2b516
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-09-28 00:51:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,21 @@
 # ******************************
 # Make your Code
 # ******************************
+# User Values
+
+numbers = []
+
+for i in range(5):
+	numbers.append(int(input()))
+
+avg = sum(numbers) / len(numbers)
+print (avg)
+
+for i in range(len(numbers)):
+	if numbers[i] > avg:
+		print ( numbers[i], end=' ')
+	# print (numbers[i], end=' ') if numbers[i] > avg else print (end='')
+
+print ()
+# newlst = [ v for v in numbers if v > avg]
+# print (newlst)
